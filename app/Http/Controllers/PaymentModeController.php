addde91fbfb94144b3208e37d87b0c7751a85dd5
GSSProjects24 <githubgrasp@gmail.com>
2025-08-18 11:03:31 +0530

    issue fixed

--- app/Http/Controllers/PaymentModeController.php
+++ app/Http/Controllers/PaymentModeController.php
@@ -70,6 +70,7 @@ class PaymentModeController extends Controller
     {
         // Get ledgers with type = 1 (Bank/Cash accounts)
         $ledgers = Ledger::where('type', 1)
+                        ->where('status', 1)
                         ->orderBy('name')
                         ->get();
 
