387185cacae7a994515960becfa22bf66de304f2
Restaurant-Grasp <restaurantgrasp@gmail.com>
2025-08-20 09:19:28 -0400

    grn dropdown changes

--- resources/views/purchase/grn/edit.blade.php
+++ resources/views/purchase/grn/edit.blade.php
@@ -136,6 +136,7 @@
                                                     data-has-serial="{{ $product->has_serial_number }}" 
                                                     data-has-warranty="{{ $product->has_warranty }}" 
                                                     data-warranty-months="{{ $product->warranty_period_months }}"
+                                                    data-default-uom="{{ $product->uom_id }}"
                                                     {{ $item->product_id == $product->id ? 'selected' : '' }}>
                                                 {{ $product->name }} ({{ $product->product_code }})
                                             </option>
@@ -368,7 +369,7 @@
             <select name="items[INDEX][product_id]" class="form-select form-select-sm product-select" required onchange="loadProductDetails(this, INDEX)">
                 <option value="">Select Product</option>
                 @foreach($products as $product)
-                <option value="{{ $product->id }}" data-has-serial="{{ $product->has_serial_number }}" data-has-warranty="{{ $product->has_warranty }}" data-warranty-months="{{ $product->warranty_period_months }}">
+                <option value="{{ $product->id }}" data-has-serial="{{ $product->has_serial_number }}" data-has-warranty="{{ $product->has_warranty }}" data-warranty-months="{{ $product->warranty_period_months }}" data-default-uom="{{ $product->uom_id }}">
                     {{ $product->name }} ({{ $product->product_code }})
                 </option>
                 @endforeach
@@ -555,7 +556,13 @@ function loadProductDetails(select, index) {
     } else {
         row.find('.serial-btn').hide();
     }
-    
+     // Set default UOM
+    const defaultUom = selectedOption.data('default-uom');
+    if(defaultUom){
+        row.find('select[name*="[uom_id]"]').val(defaultUom);
+    } else {
+        row.find('select[name*="[uom_id]"]').val('');
+    }
     updateSummary();
 }
 
